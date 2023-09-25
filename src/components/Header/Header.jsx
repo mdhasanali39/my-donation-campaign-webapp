1fafbc8825700d41fb8a8b7499bb117c14e77f2b
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-09-25 11:20:18 +0600

    Header: added bg image with overlay effect for looking good

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import Banner from "./Banner/Banner";
 import Logo from "./Logo/Logo";
 import Navbar from "./Navbar/Navbar";
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <div className="flex justify-between items-center">
+        <div className="overlay">
+            <div className="flex justify-between items-center max-w-[1300px] mx-auto px-5 pt-4">
             <Logo></Logo>
             <Navbar></Navbar>
             </div>
