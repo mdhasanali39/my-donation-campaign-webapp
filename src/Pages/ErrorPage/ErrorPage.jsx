677eef41ee60e7e80934c015d729807acf5a6bcc
mdhasanali39 <contact.hasankhanyt@gmail.com>
2023-09-27 15:34:29 +0600

    Error-Page: added errorPage with error status, here I am get the error status by using useRouteError hook, and show the status in h2 tag and text step by step with h3, h4, and one button here for go back to home

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,23 @@
+import { Link, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <h2 className="text-7xl font-bold text-red-500 mb-5">{error.status}</h2>
+      <h3 className="font-semibold text-2xl">Oops!! Page not Found</h3>
+      <h4 className="font-semibold">
+        The page you are looking for does not exist
+      </h4>
+
+      <Link to={'/'}>
+        <button className="px-5 py-3 bg-gray-700 rounded-lg text-white font-semibold mt-5">
+          Go Home
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
